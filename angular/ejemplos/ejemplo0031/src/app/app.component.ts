6b0a99ede8dc55803e5b1e3f98a485f1ae9eb12e
iconojdiazch <khepherer@gmail.com>
2017-05-06 23:10:43 +0200

    Añadido ejemplo0031 (jsonp) y ejercicio0007

---
+++ angular/ejemplos/ejemplo0031/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { InitService } from './init.service';
+import { WikiService } from './wiki.service';
+
+import {Observable} from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  private title: string = 'app works!';
+  private url: string;
+  constructor(private initService: InitService, private wikiService: WikiService) { }
+  ngOnInit(): void {
+    this.initService.getWikiUrl().subscribe(
+      data => {
+        console.log("En AppComponent");
+        console.log(data);
+        this.url = data.url;
+      }
+    );
+  }
+  private items: Observable<string[]>;
+  buscar(termino:string):void{
+    this.items = this.wikiService.buscar(termino);
+  }
+
+}
