1a6315d5dcaa672780a1f6c3cc4393b8e100c834
iconojdiazch <khepherer@gmail.com>
2017-06-28 20:40:01 +0200

    Ejemplo11 modificado

--- angular/ejemplos/ejemplo0011/src/app/app.component.ts
+++ angular/ejemplos/ejemplo0011/src/app/app.component.ts
@@ -7,13 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'app works!';
-  valores:string = '';
+  valores: string = '';
   imagenUrl: string = "http://lorempixel.com/400/200/animals";
-  activo: boolean = true;  
-  onClick(evento){
-    console.log("Pulsado",evento);
-  }    
+  activo: boolean = true;
+  esVisible: boolean = false;
+  onClick(evento) {
+    console.log("Pulsado", evento);
+  }
   onKeyUp(valor: string) {
     this.valores += valor + ' | ';
+  }
+  cambiarFondo(): void {
+    this.esVisible = !this.esVisible;
   }
 }
