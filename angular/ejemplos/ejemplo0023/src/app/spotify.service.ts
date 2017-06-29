ccd2cd6ae23c8f79b2dc2d58e21eddab46bea889
iconojdiazch <khepherer@gmail.com>
2017-06-29 23:16:40 +0200

    Asunto spotify resuelto mediante simulación

--- angular/ejemplos/ejemplo0023/src/app/spotify.service.ts
+++ angular/ejemplos/ejemplo0023/src/app/spotify.service.ts
@@ -4,9 +4,11 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SpotifyService {
-    private spotifyUrl = 'https://api.spotify.com/v1/search?q=jason&type=artist&limit=20&offset=0';
+    //private spotifyUrl = 'https://api.spotify.com/v1/search?q=jason&type=artist&limit=20&offset=0';
     constructor(private http: Http) { }
     getArtistas() {
-        return this.http.get(this.spotifyUrl).map(response => response.json());
+        //        return this.http.get(this.spotifyUrl).map(response => response.json());        
+        return this.http.get('src/app/artistas.json')
+            .map(response => response.json());
     }
 }
