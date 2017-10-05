300d31fcb7ad35aedb67520aaa752add55951f11
Usuario Genérico Aula <aula@indra.es>
2017-10-05 11:35:54 +0200

    ejemplo0037 corregido

--- angular/ejemplos/ejemplo0037/src/app/app.component.ts
+++ angular/ejemplos/ejemplo0037/src/app/app.component.ts
@@ -4,14 +4,14 @@ import { UsuarioService } from './usuario.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Usuario } from './usuario';
 import { Observable } from 'rxjs/Observable';
-import "rxjs/add/observable/of";
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
 
   subscipcionUsuario: Subscription;
@@ -20,9 +20,9 @@ export class AppComponent implements OnInit {
 
   private readonly lleganDatos: (Usuario) => void =
   u => {
-    console.log("Datos recibidos. Título:" + JSON.stringify(u.title));
+    console.log('Datos recibidos. Título:' + JSON.stringify(u.title));
     this.usuario = u;
-  };
+  }
 
   constructor(private us: UsuarioService) { }
 
